0c5b37e321309d8f61fa004ce9f48dd39c1f3fee
he <602710492@qq.com>
2016-05-29 17:19:40 +0800

    完善 research

---
+++ bricks/research-project/server.js
@@ -0,0 +1,6 @@
+exports.url = '/research-project';
+exports.get = function(req, done, fail) {
+    done({
+        research_projectActive:'active'
+    });
+};  
---
+++ bricks/server.js
@@ -0,0 +1,6 @@
+exports.url = '/research-direction';
+exports.get = function(req, done, fail) {
+    done({
+        research_directionActive:'active'
+    });
+};  
