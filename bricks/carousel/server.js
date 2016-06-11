69e2c7b362312d798fa184aa59ec9accea935fe8
he <602710492@qq.com>
2016-06-11 22:41:38 +0800

    修改了carousel，该部分目前是以修改源码的方式添加内容。首页的部分内容仍然需要改动源码修改内容。首页的部分内容暂时仍以源码形式修改内容。

--- bricks/carousel/server.js
+++ bricks/carousel/server.js
@@ -2,7 +2,18 @@ var Carousel = require('../../models/carousel');
 
 exports.get = function(req, done, fail){
     done({
-        carousels: Carousel.random(3)
+       // carousels: Carousel.random(8)
+
+        carousels:[{title: 't1',
+            content: 'content1',
+            createdTime: Date.now(),
+            img: '/img/slide/1.jpg'},{title: 't2',
+            content: 'content2',
+            createdTime: Date.now(),
+            img: '/img/slide/2.jpg'},{title: 't3',
+            content: 'content3',
+            createdTime: Date.now(),
+            img: '/img/slide/3.jpg'}]
     });
 };
 
